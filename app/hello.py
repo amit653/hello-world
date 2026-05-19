ea299004c15f5b3b2e892588bfc89f3492de1a79
amit653 <amit653@gmail.com>
2026-05-19 22:51:30 +0530

    Add files to CI workflow

--- app/hello.py
+++ app/hello.py
@@ -1,9 +1,2 @@
-version: "3.8"
-services:
-  hello:
-    image: python:3.9-slim
-    working_dir: /app
-    volumes:
-      - ./app:/app
-    command: python hello.py
+
 print("Hello World from Docker Compose on EC2!")
