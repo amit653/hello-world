35a0c09f01ddf3975564fe6f12f0d961312304c0
amit653 <amit653@gmail.com>
2026-05-22 00:32:45 +0530

    added random provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
+  bucket =  "test-${random_id.bucket_id.hex}"
+ }
+
+resource "random_id" "bucket_id"{
+byte_length=4
+   
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    random={
+source="hashicorp/random"
+version="~> 3.9.0"   # pin to a stable version
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.46.0"   # adjust to your AWS provider version
+    }
+  }
+   
+}
+
 provider "aws" {
   region = var.aws_region
 }
