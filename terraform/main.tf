fb82c72ceeb42e88afa6fc9d2d8a87f889e63a78
amit653 <amit653@gmail.com>
2026-05-22 16:34:50 +0530

    added dynamo tf table

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,18 @@
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket =  "test-${random_id.bucket_id.hex}"
+  #bucket =  "test-${random_id.bucket_id.hex}"
+  bucket =  "test-amit-2026"
  }
 
 resource "random_id" "bucket_id"{
 byte_length=4
    
+}
+terraform {
+  backend "s3" {
+    bucket         = "amit-tf-state-bucket-2026"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
 }
